4f2242ae65093295a1fe587bebbbbbc03d67237e
Wixn <iwill@nottellyou.com>
2023-08-14 12:20:00 +0200

    init

---
+++ Objects3d/MarkIV.dae.lua
@@ -0,0 +1,9 @@
+return {
+	radius = 40.0,
+	height = 40.0,
+	tex1 = "MarkIV.dds",
+	tex2 = "a7v2.dds",
+	midpos = {0,0,0},
+	mins = {-21,  0, -21},
+	maxs = { 21, 57,  21},
+}
---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+local modinfo = {
+	name = "wwi",
+	shortName = "wwi",
+	game = "wwi",
+	shortGame = "wwi",
+	mutator = "Official",
+	version = "v1.0",
+	description =	"wwi",
+	url =	"",
+	modtype = 1,
+	depend = {
+		[[rapid://zk:stable]]
+	},
+}
+
+return modinfo
